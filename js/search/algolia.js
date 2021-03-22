3da9273bbc676535e18b1e6098e1f3cd26504e40
Letian <liletian@tongji.edu.cn>
2021-03-22 23:32:13 +0800

    Site updated: 2021-03-22 23:32:12

--- js/search/algolia.js
+++ js/search/algolia.js
@@ -1,9 +1,9 @@
 $(function () {
-  const openSearch = () => {
+  $('a.social-icon.search').on('click', function () {
     $('body').css({ width: '100%', overflow: 'hidden' })
-    $('#algolia-search').css('display', 'block')
+    $('.search-dialog').css('display', 'block')
     $('.ais-search-box--input').focus()
-    $('#search-mask').fadeIn()
+    $('.search-mask').fadeIn()
     // shortcut: ESC
     document.addEventListener('keydown', function f (event) {
       if (event.code === 'Escape') {
@@ -11,43 +11,35 @@ $(function () {
         document.removeEventListener('keydown', f)
       }
     })
-  }
+  })
 
-  const closeSearch = () => {
-    $('body').css({ width: '', overflow: '' })
-    $('#algolia-search').css({
+  var closeSearch = function () {
+    $('body').css('width', '')
+    $('body').css('overflow', '')
+    $('.search-dialog').css({
       animation: 'search_close .5s'
     })
 
-    setTimeout(function () {
-      $('#algolia-search').css({
-        animation: '',
-        display: 'none'
-      })
-    }, 500)
+    $('.search-dialog').animate({}, function () {
+      setTimeout(function () {
+        $('.search-dialog').css({
+          animation: '',
+          display: 'none'
+        })
+      }, 500)
+    })
 
-    $('#search-mask').fadeOut()
+    $('.search-mask').fadeOut()
   }
+  $('.search-mask, .search-close-button').on('click touchstart', closeSearch)
 
-  const searchClickFn = () => {
-    $('a.social-icon.search').on('click', openSearch)
-    $('#search-mask, .search-close-button').on('click touchstart', closeSearch)
-  }
-
-  searchClickFn()
-
-  window.addEventListener('pjax:success', function () {
-    closeSearch()
-    searchClickFn()
-  })
-
-  const algolia = GLOBAL_CONFIG.algolia
-  const isAlgoliaValid = algolia.appId && algolia.apiKey && algolia.indexName
+  var algolia = GLOBAL_CONFIG.algolia
+  var isAlgoliaValid = algolia.appId && algolia.apiKey && algolia.indexName
   if (!isAlgoliaValid) {
     return console.error('Algolia setting is invalid!')
   }
 
-  const search = instantsearch({
+  var search = instantsearch({
     appId: algolia.appId,
     apiKey: algolia.apiKey,
     indexName: algolia.indexName,
@@ -55,7 +47,7 @@ $(function () {
       hitsPerPage: algolia.hits.per_page || 10
     },
     searchFunction: function (helper) {
-      const searchInput = $('#algolia-search-input').find('input')
+      var searchInput = $('#algolia-search-input').find('input')
 
       if (searchInput.val()) {
         helper.search()
@@ -76,7 +68,7 @@ $(function () {
       container: '#algolia-hits',
       templates: {
         item: function (data) {
-          const link = data.permalink ? data.permalink : (GLOBAL_CONFIG.root + data.path)
+          var link = data.permalink ? data.permalink : (GLOBAL_CONFIG.root + data.path)
           return (
             '<a href="' + link + '" class="algolia-hit-item-link">' +
             data._highlightResult.title.value +
@@ -102,13 +94,13 @@ $(function () {
       container: '#algolia-stats',
       templates: {
         body: function (data) {
-          const stats = GLOBAL_CONFIG.algolia.languages.hits_stats
+          var stats = GLOBAL_CONFIG.algolia.languages.hits_stats
             .replace(/\$\{hits}/, data.nbHits)
             .replace(/\$\{time}/, data.processingTimeMS)
           return (
             '<hr>' +
             stats +
-            '<span class="algolia-logo pull-right">' +
+            '<span class="algolia-logo pull_right">' +
             '  <img src="' + GLOBAL_CONFIG.root + 'img/algolia.svg" alt="Algolia" />' +
             '</span>'
           )
@@ -123,10 +115,10 @@ $(function () {
       scrollTo: false,
       showFirstLast: false,
       labels: {
-        first: '<i class="fas fa-angle-double-left"></i>',
-        last: '<i class="fas fa-angle-double-right"></i>',
-        previous: '<i class="fas fa-angle-left"></i>',
-        next: '<i class="fas fa-angle-right"></i>'
+        first: '<i class="fa fa-angle-double-left"></i>',
+        last: '<i class="fa fa-angle-double-right"></i>',
+        previous: '<i class="fa fa-angle-left"></i>',
+        next: '<i class="fa fa-angle-right"></i>'
       },
       cssClasses: {
         root: 'pagination',
@@ -138,8 +130,4 @@ $(function () {
     })
   )
   search.start()
-
-  window.pjax && search.on('render', () => {
-    window.pjax.refresh(document.getElementById('algolia-hits'))
-  })
 })
--- js/search/local-search.js
+++ js/search/local-search.js
@@ -1,13 +1,10 @@
 $(function () {
-  let loadFlag = false
-  const openSearch = function () {
-    $('body').css({
-      width: '100%',
-      overflow: 'hidden'
-    })
-    $('#local-search').css('display', 'block')
+  var loadFlag = false
+  $('a.social-icon.search').on('click', function () {
+    $('body').css({ width: '100%', overflow: 'hidden' })
+    $('.search-dialog').css('display', 'block')
     $('#local-search-input input').focus()
-    $('#search-mask').fadeIn()
+    $('.search-mask').fadeIn()
     if (!loadFlag) {
       search(GLOBAL_CONFIG.localSearch.path)
       loadFlag = true
@@ -20,37 +17,27 @@ $(function () {
         document.removeEventListener('keydown', f)
       }
     })
-  }
+  })
 
-  const closeSearch = function () {
-    $('body').css({
-      width: '',
-      overflow: ''
-    })
-    $('#local-search').css({
+  var closeSearch = function () {
+    $('body').css('width', '')
+    $('body').css('overflow', '')
+    $('.search-dialog').css({
       animation: 'search_close .5s'
     })
 
-    setTimeout(function () {
-      $('#local-search').css({
-        animation: '',
-        display: 'none'
-      })
-    }, 500)
+    $('.search-dialog').animate({}, function () {
+      setTimeout(function () {
+        $('.search-dialog').css({
+          animation: '',
+          display: 'none'
+        })
+      }, 500)
+    })
 
-    $('#search-mask').fadeOut()
+    $('.search-mask').fadeOut()
   }
-
-  const searchClickFn = () => {
-    $('a.social-icon.search').on('click', openSearch)
-    $('#search-mask, .search-close-button').on('click', closeSearch)
-  }
-
-  searchClickFn()
-  window.addEventListener('pjax:success', function () {
-    $('#local-search').is(':visible') && closeSearch()
-    searchClickFn()
-  })
+  $('.search-mask, .search-close-button').on('click touchstart', closeSearch)
 
   function search (path) {
     $.ajax({
@@ -58,39 +45,34 @@ $(function () {
       dataType: 'xml',
       success: function (xmlResponse) {
         // get the contents from search data
-        const datas = $('entry', xmlResponse).map(function () {
+        var datas = $('entry', xmlResponse).map(function () {
           return {
             title: $('title', this).text(),
             content: $('content', this).text(),
             url: $('url', this).text()
           }
         }).get()
-
-        const $input = $('#local-search-input input')[0]
-        const $resultContent = $('#local-hits')[0]
+        var $input = $('#local-search-input input')[0]
+        var $resultContent = $('#local-hits')[0]
         $input.addEventListener('input', function () {
-          let str = '<div class="search-result-list">'
-          const keywords = this.value.trim().toLowerCase().split(/[\s]+/)
+          var str = '<div class="search-result-list">'
+          var keywords = this.value.trim().toLowerCase().split(/[\s]+/)
           $resultContent.innerHTML = ''
           if (this.value.trim().length <= 0) {
             $('.local-search-stats__hr').hide()
             return
           }
-          let count = 0
+          var count = 0
           // perform local searching
           datas.forEach(function (data) {
-            let isMatch = true
-            if (!data.title || data.title.trim() === '') {
-              data.title = 'Untitled'
-            }
-            let dataTitle = data.title.trim().toLowerCase()
-            const dataContent = data.content.trim().replace(/<[^>]+>/g, '').toLowerCase()
-            const dataUrl = data.url
-            let indexTitle = -1
-            let indexContent = -1
-            let firstOccur = -1
+            var isMatch = true
+            var dataTitle = data.title.trim().toLowerCase()
+            var dataContent = data.content.trim().replace(/<[^>]+>/g, '').toLowerCase()
+            var dataUrl = data.url
+            var indexTitle = -1
+            var indexContent = -1
             // only match artiles with not empty titles and contents
-            if (dataTitle !== '' || dataContent !== '') {
+            if (dataTitle !== '' && dataContent !== '') {
               keywords.forEach(function (keyword, i) {
                 indexTitle = dataTitle.indexOf(keyword)
                 indexContent = dataContent.indexOf(keyword)
@@ -100,62 +82,21 @@ $(function () {
                   if (indexContent < 0) {
                     indexContent = 0
                   }
-                  if (i === 0) {
-                    firstOccur = indexContent
-                  }
                 }
               })
-            } else {
-              isMatch = false
             }
-
             // show search results
             if (isMatch) {
-              const content = data.content.trim().replace(/<[^>]+>/g, '')
-              if (firstOccur >= 0) {
-                // cut out 130 characters
-                let start = firstOccur - 30
-                let end = firstOccur + 100
-
-                if (start < 0) {
-                  start = 0
-                }
-
-                if (start === 0) {
-                  end = 100
-                }
-
-                if (end > content.length) {
-                  end = content.length
-                }
-
-                let matchContent = content.substring(start, end)
-
-                // highlight all keywords
-                keywords.forEach(function (keyword) {
-                  const regS = new RegExp(keyword, 'gi')
-                  matchContent = matchContent.replace(regS, '<span class="search-keyword">' + keyword + '</span>')
-                  dataTitle = dataTitle.replace(regS, '<span class="search-keyword">' + keyword + '</span>')
-                })
-
-                str += '<div class="local-search__hit-item"><a href="' + dataUrl + '" class="search-result-title">' + dataTitle + '</a>'
-                count += 1
-                $('.local-search-stats__hr').show()
-
-                if (dataContent !== '') {
-                  str += '<p class="search-result">' + matchContent + '...</p>'
-                }
-              }
-              str += '</div>'
+              str += '<div class="local-search__hit-item"><a href="' + dataUrl + '" class="search-result-title">' + dataTitle + '</a>' + '</div>'
+              count += 1
+              $('.local-search-stats__hr').show()
             }
           })
           if (count === 0) {
             str += '<div id="local-search__hits-empty">' + GLOBAL_CONFIG.localSearch.languages.hits_empty.replace(/\$\{query}/, this.value.trim()) +
               '</div>'
           }
-          str += '</div>'
           $resultContent.innerHTML = str
-          window.pjax && window.pjax.refresh($resultContent)
         })
       }
     })
